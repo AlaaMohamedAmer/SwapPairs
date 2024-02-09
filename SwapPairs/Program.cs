8f1f5b4d62cb50ae8cde9ddd1a2e98a4e9c3eef9
alaa <aamal79545@gmail.com>
2024-02-09 20:43:55 +0200

    Add project files.

---
+++ SwapPairs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapPairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class Solution
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            if (head.next == null)
+            {
+                return head;
+            }
+
+            ListNode result = SwapPairs(head.next.next);
+            ListNode output = head.next;
+            head.next.next = head;
+            head.next = result;
+            return output;
+        }
+    }
+}
